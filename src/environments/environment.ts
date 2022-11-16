b6f6aa2c21db2fb9cfdf0395398a945cd5b74aea
RolHerEV <RolHerEV@gmail.com>
2022-11-15 22:51:25 -0300

    Setup firestore

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyBUHrxi5U87Td4qWgUy5uuqgJxbiUP0B3g",
+    authDomain: "tellevoappomegaedition.firebaseapp.com",
+    projectId: "tellevoappomegaedition",
+    storageBucket: "tellevoappomegaedition.appspot.com",
+    messagingSenderId: "915551360465",
+    appId: "1:915551360465:web:9f51058deac2ff5e0bfcf0"
+  }
 };
 
 /*
